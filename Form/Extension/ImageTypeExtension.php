19bc35438438cb45476df9391667d1ac3c866a1e
Nacho Martín <nitram.ohcan@gmail.com>
2012-06-30 20:14:45 +0200

    updgrading forms to sf2.1

--- Form/Extension/ImageTypeExtension.php
+++ Form/Extension/ImageTypeExtension.php
@@ -12,9 +12,10 @@
 namespace Snowcap\ImBundle\Form\Extension;
 
 use Symfony\Component\Form\AbstractTypeExtension;
-use Symfony\Component\Form\FormBuilder;
-use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormViewInterface;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 use Snowcap\ImBundle\Manager;
 
@@ -45,31 +46,30 @@ class ImageTypeExtension extends AbstractTypeExtension
     }
 
     /**
-     * @param array $options
-     *
-     * @return array
+     * @param OptionsResolverInterface $resolver
      */
-    public function getDefaultOptions(array $options)
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        return array(
-            'format' => null,
-        );
+        $resolver->setDefaults(array(
+            'format' => null
+        ));
     }
 
     /**
-     * @param \Symfony\Component\Form\FormBuilder $builder
-     * @param array                               $options
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array                                        $options
      */
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->setAttribute('format', $options['format']);
     }
 
     /**
-     * @param \Symfony\Component\Form\FormView      $view
-     * @param \Symfony\Component\Form\FormInterface $form
+     * @param \Symfony\Component\Form\FormViewInterface      $view
+     * @param \Symfony\Component\Form\FormInterface          $form
+     * @param array                                          $options
      */
-    public function buildView(FormView $view, FormInterface $form)
+    public function buildView(FormViewInterface $view, FormInterface $form, array $options)
     {
         $format = $form->getAttribute('format');
         $imageSrc = $view->get('image_src');
