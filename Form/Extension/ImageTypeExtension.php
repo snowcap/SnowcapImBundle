93e75c230f866040749253cec2964c45c357c58b
pvanliefland <pierre@snowcap.be>
2012-07-25 19:10:11 +0200

    Adapted type extensions to Symfony 2.1beta4

--- Form/Extension/ImageTypeExtension.php
+++ Form/Extension/ImageTypeExtension.php
@@ -13,7 +13,7 @@ namespace Snowcap\ImBundle\Form\Extension;
 
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormViewInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
@@ -70,12 +70,12 @@ class ImageTypeExtension extends AbstractTypeExtension
      * @param \Symfony\Component\Form\FormView      $view
      * @param \Symfony\Component\Form\FormInterface $form
      */
-    public function buildView(FormViewInterface $view, FormInterface $form, array $options)
+    public function buildView(FormView $view, FormInterface $form, array $options)
     {
         $format = $form->getAttribute('format');
         $imageSrc = $view->get('image_src');
         if ($imageSrc !== null && $format !== null) {
-            $view->set('image_src', $this->imManager->getUrl($format, $imageSrc));
+            $view->vars['image_src'] = $this->imManager->getUrl($format, $imageSrc);
         }
     }
 }
